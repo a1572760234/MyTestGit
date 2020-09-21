e2239844ca1d6081bd46dfbc1417ff3242cdd608
刘翔宇 <a1572760234@126.com>
2020-09-21 22:57:21 +0800

    None

--- 2.py
+++ 2.py
@@ -1,3 +1,13 @@
 import time
 time.sleep(1)
 print('hello github!')
+
+def gitfunc(num):
+    '''num:循环次数'''
+    for i in range(num):
+        
+        print(f"第{i}次问候:hello git!")
+
+
+if __name__ == "__main__":
+    gitfunc(100)
